ce5df61c8f79638fdc0a63c7b793f32c5a770690
Daniela Baron <baron.daniela@gmail.com>
2014-07-01 17:36:47 -0400

    finish complex shapes lesson

--- app/scripts/gradients.js
+++ app/scripts/gradients.js
@@ -11,6 +11,7 @@
     this.width = canvas.width;
     this.height = canvas.height;
     this.drawGradients(this.ctx);
+    this.drawText(this.ctx);
 
     this.canvas.onclick = function(e) {
       console.log(e.offsetX, e.offsetY);
@@ -23,13 +24,36 @@
   TP.drawGradients = function(ctx) {
     ctx.beginPath();
     ctx.rect(20, 20, this.width-40, this.height/2 - 40);
-    var grad = ctx.createLinearGradient(20, 20, this.width-20, this.height/2 - 20);
+    // By changing size of gradient, get a different angle
+    // arguments specify begin and end points of gradient
+    // var grad = ctx.createLinearGradient(20, 20, this.width-20, this.height/2 - 20);
+    var grad = ctx.createLinearGradient(0, 0, 100, 0);
     ctx.closePath(); // must close path to avoid leaks
 
-    grad.addColorStop(0, '#000000');  // start at black
-    grad.addColorStop(1, '#aa0000');  // end at red
+    grad.addColorStop(0, '#000000');    // start at black
+    grad.addColorStop(1, '#aa0000');    // end at red
     ctx.fillStyle = grad;
     ctx.fill();
+
+    // Now create a radial gradient
+    ctx.beginPath();
+    ctx.rect(20, this.height/2, this.width-40, this.height/2-20);
+    // xcenter/ycenter/radius of first circle, then second circle
+    var radGrad = ctx.createRadialGradient(90, 360, 10, 90, 360, 300);
+    ctx.closePath();
+    radGrad.addColorStop(0, '#ffffff');
+    radGrad.addColorStop(1, '#aa0000');
+    ctx.fillStyle = radGrad;
+    ctx.fill();
+  };
+
+  TP.drawText = function(ctx) {
+    ctx.fillStyle = '#660000';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    ctx.font = 'italic 40px Arial';
+    console.log(ctx.measureText('Hello World!'));
+    ctx.fillText('Hello World!', 120, 350); // string, xpos, ypos
   };
 
   TP.setLineStyle = function(ctx, width, color, cap, join) {
